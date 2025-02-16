86c4527103c7341146cdcc9f51c657b564f15dc1
jerrbearis2cool <68572439+jerrbearis2cool@users.noreply.github.com>
2025-02-16 10:06:51 -0600

    new build.py

--- build.py
+++ build.py
@@ -1,12 +1,7 @@
 import os
 import PyInstaller.__main__
 
-# run this to make an exe
 script_path = os.path.join(os.getcwd(), "main.py")
-
-# make an ico
-icon_path = os.path.join(os.getcwd(), "assets\\icon.ico")
-
 output_dir = os.path.join(os.getcwd(), "dist")
 
 PyInstaller.__main__.run(
@@ -14,12 +9,8 @@ PyInstaller.__main__.run(
         script_path,
         "--onefile",
         "--windowed",
-        "--icon=" + icon_path,
-        "--add-data",
-        f"{icon_path};.",
         "--distpath",
         output_dir,
-        # change the name
         "--name",
         "SaveTheAnimals",
     ]
